b3d55ec424bea154a3bbf051665d04cfa438ed36
t1mn0 <timofeyalbecov@gmail.com>
2025-10-17 19:26:49 +0300

    first_party: AnyErr has been added in first-party

--- first_party/include/throwless/Error.hpp
+++ first_party/include/throwless/Error.hpp
@@ -20,4 +20,28 @@ concept Error = requires(const E& e, E&& moved) {
   requires std::equality_comparable<E>;
 };
 
+struct AnyErr {
+protected:
+  std::string msg_;
+
+public:
+  AnyErr() = default;
+  AnyErr(const std::string& msg) : msg_(msg) {}
+  AnyErr(const char* msg) : msg_(msg) {}
+
+  template <typename...Args> requires std::constructible_from<std::string, Args...>
+  AnyErr(Args&&... args) : msg_(args...) {}
+
+  virtual ~AnyErr() = default;
+
+  virtual std::string err_msg() const { return msg_; }
+  virtual const char* what() const noexcept { return msg_.c_str(); }
+
+  virtual bool operator==(const AnyErr& oth) const noexcept {
+    return msg_ == oth.msg_;
+  }
+};
+
+static_assert(tmn::err::Error<AnyErr>, "AnyErr must satisfy Error concept");
+
 } // namespace tmn::err;
